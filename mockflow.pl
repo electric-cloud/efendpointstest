29d15bdd644cac3c946a0ea37803e6e094de3bc0
Greg Maxey <gmaxey@electric-cloud.com>
2018-10-23 14:11:39 -0400

    Pass headers and body to called DSL

--- mockflow.pl
+++ mockflow.pl
@@ -5,27 +5,39 @@ use HTTP::Daemon;
 use JSON;
 use Data::Dumper;
 
+sub TO_JSON { return { %{ shift() } }; }
+
 $SIG{CHLD} = 'IGNORE';
 
 my $d = HTTP::Daemon->new(LocalPort => 8888) || die;
 print "Please contact me at: <URL:", $d->url, ">\n";
-
-
 while (my $c = $d->accept) {
     unless (fork()) {
-        my $username = $ENV{MOCKFLOW_USER} ||= 'admin';
-        my $password = $ENV{MOCKFLOW_PASS} ||= 'changeme';
-        my $host = $ENV{MOCKFLOW_EF_HOST} ||= 'localhost';
-        my $ec = new ElectricCommander({server => $host});
-        $ec->login($username, $password);
-        print "Logged in successfully\n";
+        my $ec = new ElectricCommander();
+        $ec->login('admin','changeme');
 
         while (my $r = $c->get_request) {
             if ($r->uri->path eq "/test" or $r->method eq 'POST' and $r->uri->path eq "/endpoints/EC-Github/1.0/webhook") {
                 my $jobId = eval {
                     my $prop = $ec->getProperty("/myPlugin/project/ec_endpoints/webhook/dsl", {pluginName => "EC-Github"});
                     my $dsl = $prop->findvalue("//value")->value();
-                    my $response = $ec->evalDsl($dsl);
+					
+					my $headers = encode_json TO_JSON ($r->headers);
+					my $payload = $r->content;
+					print STDERR "\n" . "Payload: " . $payload;
+					print STDERR "\n" . "Headers: " . $headers;
+
+					my $response = $ec->evalDsl(
+						$dsl, {
+							parameters => qq(
+								{
+									"headers": $headers,
+									"payload": $payload
+								}
+							)
+						},
+					);
+				
                     $response->findvalue("//jobId")->value();
                 };
                 if ($@) {
@@ -33,7 +45,7 @@ while (my $c = $d->accept) {
                     $c->send_header( "Content-type", "text/plain" );
                     printf $c "\nEncountered error:\n%s\n", $@;
                 } else {
-                    printf STDERR "JobID: %s\n", $jobId;
+                    printf STDERR "\nJobID: %s\n", $jobId;
                     $c->send_status_line;
                     $c->send_header( "Content-type", "text/plain" );
                     printf $c "\nJobID: %s\n", $jobId;
